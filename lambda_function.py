ab63b0adef8be36836b8ad9d44067ac22521ea2a
Narjis-ali210 <noonie1273@gmail.com>
2026-06-09 00:14:41 +0300

    lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -9,21 +9,17 @@ def lambda_handler(event, context):
     dynamodb = boto3.resource('dynamodb')
     table = dynamodb.Table('imaqanalysis')
     
-    # اسم الصورة من الـ event مع فك التشفير
     bucket = event['Records'][0]['s3']['bucket']['name']
     raw_key = event['Records'][0]['s3']['object']['key']
     image_name = urllib.parse.unquote_plus(raw_key)
     
-    # تحليل الصورة باستخدام Rekognition
     response = rekognition_client.detect_labels(
         Image={'S3Object': {'Bucket': bucket, 'Name': image_name}},
         MaxLabels=10
     )
     
-    # استخراج التسميات
     labels = [label['Name'] for label in response['Labels']]
     
-    # حفظ النتائج في DynamoDB
     table.put_item(Item={
         'ImageName': image_name,
         'Labels': json.dumps(labels),
